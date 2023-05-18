d7c17f35367c36dbb0803919044499a1f493206c
TK <t.koprivnik@gmail.com>
2023-05-18 16:13:48 +0200

    Add hamburger functionality for Header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,32 +1,38 @@
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 import styles from './Header.module.scss';
 
 const Header = () => {
-  console.log(styles);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header>
-      <div className={styles.menuBtn}>
-        <span className={styles.menuBtnBurger}></span>
+      <div className={isOpen ? `${styles.menuBtn} ${styles.open}` : styles.menuBtn} onClick={handleMenuClick}>
+        <span className={isOpen ? `${styles.menuBtnBurger} ${styles.open}` : styles.menuBtnBurger}></span>
       </div>
 
-      <nav>
-        <ul className={styles.menuNav}>
-          <li className={styles.menuNavItem}>
+      <nav className={isOpen ? `${styles.open}` : ''}>
+        <ul className={isOpen ? `${styles.menuNav} ${styles.open}` : styles.menuNav}>
+          <li className={isOpen ? `${styles.menuNavItem} ${styles.open}` : styles.menuNavItem}>
             <NavLink exact to="/" className={styles.menuNavLink} activeClassName={styles.active}>
               Home
             </NavLink>
           </li>
-          <li className={styles.menuNavItem}>
+          <li className={isOpen ? `${styles.menuNavItem} ${styles.open}` : styles.menuNavItem}>
             <NavLink to="/about" className={styles.menuNavLink} activeClassName={styles.active}>
               About Me
             </NavLink>
           </li>
-          <li className={styles.menuNavItem}>
+          <li className={isOpen ? `${styles.menuNavItem} ${styles.open}` : styles.menuNavItem}>
             <NavLink to="/projects" className={styles.menuNavLink} activeClassName={styles.active}>
               My Projects
             </NavLink>
           </li>
-          <li className={styles.menuNavItem}>
+          <li className={isOpen ? `${styles.menuNavItem} ${styles.open}` : styles.menuNavItem}>
             <NavLink to="/contact" className={styles.menuNavLink} activeClassName={styles.active}>
               Contact Me
             </NavLink>
