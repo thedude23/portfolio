20288dda19ff9ffd9bdf832b845a1dd9b86b24da
TK <t.koprivnik@gmail.com>
2023-05-19 17:45:46 +0200

    Add rel attribute to links

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -5,19 +5,19 @@ const Footer = () => {
   return (
     <footer>
       <section className={styles.socialIcons}>
-        <a href="https://twitter.com/TimKoprivnik" target="blank">
+        <a href="https://twitter.com/TimKoprivnik" target="_blank" rel="noreferrer noopener">
           <FaTwitter />
         </a>
-        <a href="https://www.facebook.com/tix00" target="blank">
+        <a href="https://www.facebook.com/tix00" target="_blank" rel="noreferrer noopener">
           <FaFacebookF />
         </a>
-        <a href="https://www.instagram.com/tim.koprivnik/" target="blank">
+        <a href="https://www.instagram.com/tim.koprivnik/" target="_blank" rel="noreferrer noopener">
           <FaInstagram />
         </a>
-        <a href="https://github.com/tim-koprivnik" target="blank">
+        <a href="https://github.com/tim-koprivnik" target="_blank" rel="noreferrer noopener">
           <FaGithub />
         </a>
-        <a href="https://www.linkedin.com/in/tim-koprivnik-0238b4147/" target="blank">
+        <a href="https://www.linkedin.com/in/tim-koprivnik-0238b4147/" target="_blank" rel="noreferrer noopener">
           <FaLinkedinIn />
         </a>
       </section>
