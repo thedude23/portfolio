6789c5e63951c85c520533f299a86302adcb56ab
TK <t.koprivnik@gmail.com>
2023-05-22 11:56:08 +0200

    Update ErrorPage component

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,22 +1,11 @@
-import { useRouteError, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { IoArrowBackOutline } from 'react-icons/io5';
 import PageTemplate from '../templates/PageTemplate';
 import MainContent from '../layout/MainContent';
 import styles from './ErrorPage.module.scss';
 
-function ErrorPage() {
-  const error = useRouteError();
+const ErrorPage = () => {
   const navigate = useNavigate();
-
-  let message = 'Something went wrong.';
-
-  if (error.status === 500) {
-    message = error.data.message;
-  }
-
-  if (error.status === 404) {
-    message = 'The page you are looking for could not be found.';
-  }
 
   const goBackHandler = () => {
     navigate(-1);
@@ -27,7 +16,7 @@ function ErrorPage() {
       mainContent={
         <MainContent className={styles.errorPage}>
           <h1>Oops!</h1>
-          <p>{message}</p>
+          <p>Something went wrong.</p>
           <button type="button" onClick={goBackHandler}>
             <IoArrowBackOutline size={18} />
             Go Back
@@ -36,6 +25,6 @@ function ErrorPage() {
       }
     />
   );
-}
+};
 
 export default ErrorPage;
