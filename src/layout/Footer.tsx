4e6f2030548ec9029f811da57d9942a3b5bbf86e
TK <t.koprivnik@gmail.com>
2023-05-22 10:52:47 +0200

    Update Footer component

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,42 @@
+import styles from './Footer.module.scss';
+import { FaTwitter, FaFacebookF, FaInstagram, FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+interface Props {}
+
+const Footer: React.FC<Props> = () => {
+  return (
+    <footer>
+      <section className={styles.socialIcons}>
+        <a href="https://twitter.com/TimKoprivnik" target="_blank" rel="noreferrer noopener" aria-label="Twitter">
+          <FaTwitter />
+        </a>
+        <a href="https://www.facebook.com/tix00" target="_blank" rel="noreferrer noopener" aria-label="Facebook">
+          <FaFacebookF />
+        </a>
+        <a
+          href="https://www.instagram.com/tim.koprivnik/"
+          target="_blank"
+          rel="noreferrer noopener"
+          aria-label="Instagram"
+        >
+          <FaInstagram />
+        </a>
+        <a href="https://github.com/tim-koprivnik" target="_blank" rel="noreferrer noopener" aria-label="Github">
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/tim-koprivnik-0238b4147/"
+          target="_blank"
+          rel="noreferrer noopener"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedinIn />
+        </a>
+      </section>
+
+      <div className={styles.copyright}>&copy; Copyright {new Date().getFullYear()}</div>
+    </footer>
+  );
+};
+
+export default Footer;
