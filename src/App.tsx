8cae3bb888a075d8b0f24deeb872726794e25cda
TK <t.koprivnik@gmail.com>
2023-05-25 15:27:35 +0200

    Update analytics to ga4

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,19 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import ProjectsPage from './pages/ProjectsPage';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
+
+const GA_MEASUREMENT_ID = import.meta.env.VITE_REACT_APP_GA_MEASUREMENT_ID;
+ReactGA.initialize(GA_MEASUREMENT_ID);
 
 const RoutesComponent = () => {
   const location = useLocation();
 
   useEffect(() => {
-    ReactGA.pageview(location.pathname + location.search);
+    ReactGA.send({
+      hitType: 'pageview',
+      page_path: location.pathname + location.search,
+    });
   }, [location]);
 
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
-import ReactGA from 'react-ga';
-
-const GA_TRACKING_ID = import.meta.env.VITE_REACT_APP_GA_TRACKING_ID;
-
-ReactGA.initialize(GA_TRACKING_ID);
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
