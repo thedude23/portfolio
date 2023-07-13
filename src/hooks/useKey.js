88ac3e81c52548c46ed736c22d4eca831baad4aa
TK <t.koprivnik@gmail.com>
2023-07-13 13:55:31 +0200

    Add more custom hooks

---
+++ src/hooks/useKey.js
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+
+function useKey(key, action) {
+  useEffect(() => {
+    const callback = (e) => {
+      if (e.code.toLowerCase() === key.toLowerCase()) {
+        action();
+      }
+    };
+
+    document.addEventListener('keydown', callback);
+
+    return () => {
+      document.removeEventListener('keydown', callback);
+    };
+  }, [action, key]);
+}
+
+export default useKey;
