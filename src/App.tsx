97f3067cad73e2fb0edd6e1cd4fb414d78225948
Tim Koprivnik <tim.koprivnik@agiledrop.com>
2023-10-04 13:14:11 +0200

    Remove GA tracking

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,20 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import ProjectsPage from './pages/ProjectsPage';
-import ReactGA from 'react-ga4';
+// import ReactGA from 'react-ga4';
 
-const GA_MEASUREMENT_ID = import.meta.env.VITE_REACT_APP_GA_MEASUREMENT_ID;
-ReactGA.initialize(GA_MEASUREMENT_ID);
+// const GA_MEASUREMENT_ID = import.meta.env.VITE_REACT_APP_GA_MEASUREMENT_ID;
+// ReactGA.initialize(GA_MEASUREMENT_ID);
 
 const RoutesComponent = () => {
-  const location = useLocation();
+  // const location = useLocation();
 
-  useEffect(() => {
-    ReactGA.send({
-      hitType: 'pageview',
-      page_path: location.pathname + location.search,
-    });
-  }, [location]);
+  // useEffect(() => {
+  //   ReactGA.send({
+  //     hitType: 'pageview',
+  //     page_path: location.pathname + location.search,
+  //   });
+  // }, [location]);
 
   return (
     <Routes>
